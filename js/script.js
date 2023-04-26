607ccfc10d3d60cc90847a97dc972790c60b8974
Juulkeuuuh <90319980+Juulkeuuuh@users.noreply.github.com>
2023-04-26 23:09:14 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 
 $(document).ready(function(){
-
+  var viewportWidth = $(window).width();
   // $(document).mousemove(function(event) {
   //   $('.blob').css({
   //     'left': event.pageX,
@@ -15,5 +15,63 @@ $(document).ready(function(){
       'top': event.pageY
     }, 30, 'linear');
   });
+  if (viewportWidth < 500) {
+    $('.planet').css({
+      'transition': 'all ease 1000ms',
+      'width': '100vw',
+    })
+  } else {
+    $('.planet').css({
+      'transition': 'all ease 1000ms',
+      'width': '50vw',
+    })
+  }
+
+
+  setTimeout(function() {
+    $('#mainLogo').css({
+    'left': '5%'
+  })
+  }, 2000);
+  setTimeout(function() {
+    $('#mainScrib').css({
+    'translate': '0 0'
+  })
+  }, 2500);
+  setTimeout(function() {
+    $('#mainDate').css({
+    'left': '5%'
+  })
+  }, 2500);
+
+  setTimeout(function() {
+    $('.continue').css({
+    'width': '300px'
+   })
+   $('.onTop').remove();
+  }, 2500);
+
+
+
+$('.beneath').click(function () {
+    $('.planet').css({
+    'width': '1000vw',
+    'opacity': '0%'
+  })
+
+  setTimeout(function() {
+    $('.loadLayer').css({
+    'opacity': '0%'
+  })
+  }, 300);
+
+  setTimeout(function() {
+    $('.loadLayer').remove();
+  }, 300);
+
+})
+
+
+
 
 })
